afe16756b07bf9c03a3816a3c7f28ae2c773ac9b
shofujimoto0617 <shofujimoto017@gmail.com>
2020-12-28 10:56:13 +0900

    add about

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
